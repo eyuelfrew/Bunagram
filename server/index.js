6ec6c06eb9fc16dca291e407b59255ab15cb981e
Eyuel Frew <eyuelfre430@gmail.com>
2024-09-03 12:00:47 +0300

    fixed: CORS issue

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ import UserModel from "./models/UserModels.js";
 dotenv.config();
 const app = express();
 const corsOptions = {
-  origin: ["https://frontend.amtprinting19.com"],
+  origin: ["https://bunagram-1.onrender.com"],
   credentials: true,
 };
 app.use(bodyParser.urlencoded({ extended: true }));
